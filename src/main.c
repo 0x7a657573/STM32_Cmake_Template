24cc1c497f59b31ab9900c264ce195f335771e1a
mohammad mazarei <mohammad.mazarei@gmail.com>
2023-03-15 09:57:18 +0330

    init poject

---
+++ src/main.c
@@ -0,0 +1,25 @@
+/**********************************************************************
+ * File : main.c
+ * Copyright (c) 0x7a657573.
+ * Created On : Mon Mar 13 2023
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+
+
+int main(void)
+{
+
+   return 0; 
+}
