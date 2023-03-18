a37add018cc11f9cf5b6d70af192601855178861
mohammad mazarei <mohammad.mazarei@gmail.com>
2023-03-18 17:00:54 +0330

    add flag USE_RTOS in init HAL

--- src/initialize-hardware.c
+++ src/initialize-hardware.c
@@ -92,7 +92,7 @@ __initialize_hardware(void)
 }
 
 // Disable when using RTOSes, since they have their own handler.
-#if 1
+#ifndef USE_RTOS
 
 // This is a sample SysTick handler, use it if you need HAL timings.
 void __attribute__ ((section(".after_vectors")))
