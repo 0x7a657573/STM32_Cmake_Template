a43cbe559660790e09b4c18497ddec14d21e5e56
mohammad mazarei <mohammad.mazarei@gmail.com>
2023-04-04 13:24:21 +0330

    add f0 bsp layer

--- BSP/bsp.h
+++ BSP/bsp.h
@@ -48,7 +48,7 @@
 #include <stm32f0xx_ll_utils.h>
 #include <stm32f0xx_ll_gpio.h>
 
-#define LED_PIN                           LL_GPIO_PIN_12
+#define LED_PIN                           LL_GPIO_PIN_13
 #define LED_GPIO_PORT                     GPIOC
 #define LED_GPIO_CLK_ENABLE()             LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC)
 
